6738c2387a4a462977b0d52170f71553d186ba53
Bira Botto <tibira.botto@gmail.com>
2024-11-24 18:46:49 -0500

    Config project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+const theme = extendTheme({
+  config,
+  colors: {
+    barber: {
+      900: "#12131b",
+      400: "#1b1c29",
+      100: "#c6c6c6",
+    },
+    button: {
+      cta: "#fba931",
+      default: "#FFF",
+      gray: "#dfdfdf",
+      danger: "#ff4040",
+    },
+    orange: {
+      900: "#fba931",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
