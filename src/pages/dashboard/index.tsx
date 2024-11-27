798fed4d96ca76d0c2f8be83136eaec8bd9b7fa0
Bira Botto <tibira.botto@gmail.com>
2024-11-27 09:09:51 -0500

    URLS secure

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Flex, Text } from "@chakra-ui/react";
+import { canSSRAuth } from "@/utils/canSSRAuth";
 
 export default function Dashboard() {
   return (
@@ -13,3 +14,9 @@ export default function Dashboard() {
     </>
   );
 }
+
+export const getServerSideProps = canSSRAuth(async () => {
+  return {
+    props: {},
+  };
+});
