a714dc5e7ff46f1ddbb9c768ad44208789c6248e
Bira Botto <tibira.botto@gmail.com>
2024-11-29 12:53:02 -0500

    Create edit haircut

--- src/pages/haircuts/[id].tsx
+++ src/pages/haircuts/[id].tsx
@@ -1,4 +1,6 @@
 import { SideBar } from "@/components/sidebar";
+import { canSSRAuth } from "@/utils/canSSRAuth";
+import { setupAPIClient } from "@/services/api";
 import {
   Flex,
   Text,
@@ -12,9 +14,60 @@ import {
 import Head from "next/head";
 import Link from "next/link";
 import { FiChevronLeft } from "react-icons/fi";
+import { ChangeEvent, useState } from "react";
 
-export default function EditHaircut() {
+interface HaircutItem {
+  id: string;
+  name: string;
+  price: string | number;
+  status: boolean;
+}
+
+interface SubscriptionItem {
+  id: string;
+  status: string;
+}
+
+interface EditHaircutProps {
+  haircut: HaircutItem;
+  subscription: SubscriptionItem;
+}
+
+export default function EditHaircut({
+  subscription,
+  haircut,
+}: EditHaircutProps) {
   const [isMobile] = useMediaQuery("(max-width: 500px)");
+  const [name, setName] = useState(haircut?.name);
+  const [price, setPrice] = useState(haircut?.price);
+  const [status, setStatus] = useState(haircut?.status);
+  const [disableHaircut, setDisableHaircut] = useState(
+    haircut?.status ? "disabled" : "enabled"
+  );
+  async function handleEdit() {
+    if (name === "" || price === "") return;
+    try {
+      const apiClient = setupAPIClient();
+      await apiClient.put("/haircuts", {
+        name,
+        price: Number(price),
+        status,
+        haircut_id: haircut?.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleChangeStatus(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.value === "disabled") {
+      setDisableHaircut("enabled");
+      setStatus(false);
+    } else {
+      setDisableHaircut("disabled");
+      setStatus(true);
+    }
+  }
   return (
     <>
       <Head>
@@ -67,6 +120,8 @@ export default function EditHaircut() {
                 type="text"
                 w="100%"
                 color="white"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <Input
                 placeholder="Name of price ex. 45.90"
@@ -76,11 +131,21 @@ export default function EditHaircut() {
                 type="number"
                 w="100%"
                 color="white"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
               />
 
               <Stack mb={6} align="center" direction="row">
                 <Text color="white">Disable</Text>
-                <Switch size="lg" colorScheme="red" />
+                <Switch
+                  size="lg"
+                  colorScheme="red"
+                  value={disableHaircut}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleChangeStatus(e)
+                  }
+                  isChecked={disableHaircut === "disabled" ? false : true}
+                />
               </Stack>
               <Button
                 w="100%"
@@ -88,9 +153,27 @@ export default function EditHaircut() {
                 mb={6}
                 bg="button.cta"
                 _hover={{ bg: "#FFb13c" }}
+                disabled={subscription.status !== "active"}
+                onClick={handleEdit}
               >
                 Edit
               </Button>
+
+              {subscription?.status !== "active" && (
+                <Flex direction="row" align="center" justify="center">
+                  <Link href="/plans">
+                    <Text
+                      color="#31fb6a"
+                      cursor="pointer"
+                      fontWeight="bold"
+                      mr={1}
+                    >
+                      Get Premium
+                    </Text>
+                  </Link>
+                  <Text color="white">and get full access</Text>
+                </Flex>
+              )}
             </Flex>
           </Flex>
         </Flex>
@@ -98,3 +181,31 @@ export default function EditHaircut() {
     </>
   );
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const check = await apiClient.get("/haircut/check");
+    const response = await apiClient.get("/haircut/detail", {
+      params: {
+        haircut_id: id,
+      },
+    });
+
+    return {
+      props: {
+        haircut: response.data,
+        subscription: check.data?.subscriptions,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+});
